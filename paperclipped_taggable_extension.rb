27559706a3a3ac2d997c6295fd3622f0c6bf29bc
will <will@spanner.org>
2009-09-10 12:22:11 +0100

    extension requirements declared

--- paperclipped_taggable_extension.rb
+++ paperclipped_taggable_extension.rb
@@ -3,10 +3,14 @@ class PaperclippedTaggableExtension < Radiant::Extension
   description "Assign tags to paperclipped assets and display galleries based on tags, page, or whatever you like"
   url "http://spanner.org/radiant/paperclipped_taggable"
   
+  extension_config do |config|
+    config.extension 'paperclipped'
+  end
+  
   def activate
     Asset.send :is_taggable                                              # make pages taggable 
-    Asset.send :include, TaggableAsset                                   # and add a keywords method for compatibility with pages
-    Page.send :include, AssetGalleryTags
+    Asset.send :include, TaggableAsset                                   # add a keywords method for compatibility with pages
+    Page.send :include, AssetGalleryTags                                 # and a load of new page tags for displaying galleries and lists of assets
   end
   
 end
